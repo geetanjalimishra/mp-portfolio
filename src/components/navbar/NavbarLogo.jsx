ac0a87c78942ed9afb246d46bab95e9eca5a29a8
Geetanjali Mishra <137869865+geetanjalimishra@users.noreply.github.com>
2025-06-29 16:22:32 +0530

    Update NavbarLogo.jsx

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -7,7 +7,7 @@ const NavbarLogo = () => {
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
         MP
       </h1> */}
-      <img src="/public/images/mp-logo1.png" alt="logo" className="w-[135px] h-[100px]"></img>
+      <img src="/images/mp-logo1.png" alt="logo" className="w-[135px] h-[100px]"></img>
     </div>
   );
 };
